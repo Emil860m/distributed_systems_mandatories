a087ba40aeeb5dc6373e52bee3f69f68b039181e
Frederik B <spicyoverlord@gmail.com>
2023-09-22 14:25:51 +0200

    stiff stuff stoff

--- mandatory2.go
+++ mandatory2.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
-	"time"
 )
 
 func main() {
@@ -17,40 +17,80 @@ func main() {
 	<-clientChannel
 }
 func server(completeChannel chan bool) {
-	listener, err := net.Listen("tcp", "127.0.0.1:8080")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer listener.Close()
-
+	lConn := createUDPListener(8080)
 	fmt.Println("Server listening on 127.0.0.1:8080")
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer conn.Close()
+	x, remote := receiveIntFromConn(lConn)
+	fmt.Printf("Server recieved: %d from %s\n", x, remote)
 
-	fmt.Println("Connection established with client")
-
+	fmt.Println("Server finished!")
 	completeChannel <- true
-	// Handle incoming data or perform actions here
 }
 
 func client(completeChannel chan bool) {
-	time.Sleep(time.Second * 4)
+	wConn := createUDPWriter(8081, 8080)
+	lConn := createUDPListener(8081)
 
-	conn, err := net.Dial("tcp", "127.0.0.1:8080")
-	if err != nil {
-		fmt.Println("Error:", err)
+	// first send from client
+	var x uint32 = 10
+
+	sendIntToConn(x, wConn)
+	fmt.Printf("Client sent: %d\n", x)
+
+	x1, remote := receiveIntFromConn(lConn)
+
+	if x1 == x+1 {
+
+	}
+
+	fmt.Println("Client finished!")
+	completeChannel <- true
+}
+
+func sendIntToConn(i uint32, wConn *net.UDPConn) {
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, i)
+	_, writeErr := wConn.Write(bs)
+	if writeErr != nil {
+		fmt.Println("failed:", writeErr)
 		return
 	}
-	defer conn.Close()
+}
+func receiveIntFromConn(lConn *net.UDPConn) (uint32, *net.UDPAddr) {
+	bs := make([]byte, 4)
+	_, remote, _ := lConn.ReadFromUDP(bs[:])
+	x := binary.LittleEndian.Uint32(bs)
 
-	fmt.Println("Connected to server")
+	return x, remote
+}
 
-	completeChannel <- true
-	// Perform any necessary actions here or send data to the server
+func createUDPListener(port int) *net.UDPConn {
+	addr := net.UDPAddr{
+		Port: port,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	return conn
+}
+func createUDPWriter(lPort int, rPort int) *net.UDPConn {
+	raddr := net.UDPAddr{
+		Port: rPort,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	laddr := net.UDPAddr{
+		Port: lPort,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	conn, err := net.DialUDP("udp", &laddr, &raddr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	return conn
 }
